d6dd6e76a967c08e216837d231f834ccc3020c63
shengbinmeng <shengbinmeng@gmail.com>
2013-01-31 23:53:01 +0800

    change .gitignore to ignore .DS_Store on Mac OS X; delete libs/

--- ChiSha/src/pku/sigroup/chisha/ChooseActivity.java
+++ ChiSha/src/pku/sigroup/chisha/ChooseActivity.java
@@ -2,73 +2,98 @@ package pku.sigroup.chisha;
 
 import java.util.Arrays;
 import java.util.List;
-
+import com.actionbarsherlock.app.SherlockFragment;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import pku.shengbin.utils.MessageBox;
 import pku.tangkai.utils.ShakeListener;
-
-import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class ChooseActivity extends Activity {
-	private List<String> mChoiceList;
-	private ShakeListener mShaker;
-	String[] canteen;
-	TextView text;
-	int last;
+public class ChooseActivity extends SherlockFragmentActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.choose); 
-		mChoiceList = ChoiceManager.getChoiceList();
-		// add some default choices if empty (only first time)
-		if (mChoiceList.isEmpty()) {
-			String[] canteens = this.getResources().getStringArray(R.array.canteen);
-			mChoiceList.addAll(Arrays.asList(canteens));
-		}
-		
-		text = (TextView) findViewById(R.id.choose);
-		canteen = mChoiceList.toArray(new String[0]);
-		
-		final Vibrator vibe = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
-		
-	    //mShaker = new ShakeListener(this);
-	    /*
-	    mShaker.setOnShakeListener(new ShakeListener.OnShakeListener () {
-		    public void onShake() {
-		    	int r;
-		    	do {
-		    		r =  (int)(Math.random() * canteen.length);
-		    	} while(r==last);
-		    	last = r;
-		    	text.setText(canteen[r]);
-		    	vibe.vibrate(100);
-		    }
-	    });
-	    */
-	}
-	
-	@Override
-	protected void onResume() {
-		text.setText(R.string.welcome);
-		//TODO: should handle the exception here
-		//mShaker.onResume();
-		
-		super.onResume();
-	}
-	
-	@Override
-	protected void onPause() {
-	    //mShaker.onPause();
-	    super.onPause();
-	}
+		FragmentManager fm = getSupportFragmentManager();
 
-	@Override
-    protected void onStop(){
-		//mShaker.onStop();
-    	super.onStop();
+        // Create the fragment and add it as our sole content.
+        if (fm.findFragmentById(android.R.id.content) == null) {
+            ChooseFragment choose = new ChooseFragment();
+            fm.beginTransaction().add(android.R.id.content, choose).commit();
+        }		
+	}
+	
+	
+	public static class ChooseFragment extends SherlockFragment {
+		private List<String> mChoiceList;
+		private ShakeListener mShaker;
+		String[] canteen;
+		TextView text;
+		int last;
+		
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+        		Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.choose, container, false);
+    		text = (TextView) v.findViewById(R.id.choose);
+    		mChoiceList = ChoiceManager.getChoiceList();
+    		// add some default choices if empty (only first time)
+    		if (mChoiceList.isEmpty()) {
+    			String[] canteens = this.getResources().getStringArray(R.array.canteen);
+    			mChoiceList.addAll(Arrays.asList(canteens));
+    		}
+    		
+    		canteen = mChoiceList.toArray(new String[0]);
+    		
+    		final Vibrator vibe = (Vibrator)this.getActivity().getSystemService(Context.VIBRATOR_SERVICE);
+    		try {
+	    	    mShaker = new ShakeListener(this.getActivity());
+	    	    mShaker.setOnShakeListener(new ShakeListener.OnShakeListener () {
+	    		    public void onShake() {
+	    		    	int r;
+	    		    	do {
+	    		    		r =  (int)(Math.random() * canteen.length);
+	    		    	} while(r==last);
+	    		    	last = r;
+	    		    	text.setText(canteen[r]);
+	    		    	vibe.vibrate(100);
+	    		    }
+	    	    });
+    		} catch (Exception e) {
+    			MessageBox.show(this.getActivity(), "Sorry", e.getMessage());
+    		}
+    	    
+            return v;
+        }
+        
+        @Override
+		public void onResume() {
+    		text.setText(R.string.welcome);
+    		if (mShaker != null)
+    			mShaker.onResume();
+    		super.onResume();
+    	}
+    	
+    	@Override
+		public void onPause() {
+    		if (mShaker != null)
+    			mShaker.onPause();
+    	    super.onPause();
+    	}
+
+    	@Override
+		public void onStop() {
+    		if (mShaker != null)
+    			mShaker.onStop();
+        	super.onStop();
+        }
+       
     }
 	
 }
